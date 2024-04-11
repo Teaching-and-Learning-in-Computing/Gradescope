125f8ea4724bc0dcb152908a942031ccfe104261
Nitro <admin@nitrostudio.dev>
2024-04-10 19:36:52 -0700

    Add `get_courses` function.

--- gradescope/gradescope.py
+++ gradescope/gradescope.py
@@ -113,6 +113,37 @@ class Gradescope:
                 raise ValueError('Unknown return URL.')
         return False
 
+    def get_courses(self):
+        '''
+        Retrieves the courses from the Gradescope.
+
+        Returns:
+            dict: A dictionary containing the courses categorized as 'Instructor Courses' and 'Student Courses'.
+                  Each category contains a list of course details including course ID, URL, term, short name, and full name.
+        '''
+        response = self.session.get(BASE_URL)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        courses_dict = {'Instructor Courses': [], 'Student Courses': []}
+        for heading in courses_dict.keys():
+            current_heading = soup.find('h1', string=heading)
+            if current_heading:
+                course_lists = current_heading.find_next_sibling('div', class_='courseList')
+                for term in course_lists.find_all(class_='courseList--term'):
+                    term_name = term.get_text(strip=True)
+                    courses_container = term.find_next_sibling(class_='courseList--coursesForTerm')
+                    if courses_container:
+                        for course in courses_container.find_all(class_='courseBox'):
+                            if course.name == 'a':
+                                courses_dict[heading].append({
+                                    'course_id': course.get('href', '').split('/')[-1],
+                                    'url': f'{BASE_URL}{course.get("href", None)}',
+                                    'term': term_name,
+                                    'short_name': course.find(class_='courseBox--shortname').get_text(strip=True),
+                                    'full_name': course.find(class_='courseBox--name').get_text(strip=True),
+                                })
+        return courses_dict
+
     def _response_check(self, response: requests.Response) -> bool:
         '''
         Checks if the HTTP response is successful.
--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import json
-from gradescope import Gradescope
+from gradescope import Gradescope, save_json
 
 with open('./login.key', 'r') as f:
     login_info = json.load(f)
 # print(login_info)
 
 gs = Gradescope(login_info['username'], login_info['password'], verbose=True)
+courses = gs.get_courses()
+save_json('./courses.json', courses)
