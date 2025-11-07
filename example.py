dc2d760535d0a0751dd55620cb76c2bc44a01bfe
rhit-crossjd <crossjd@rose-hulman.edu>
2025-11-07 09:45:25 -0500

    put example back to what it was

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ with open('./login.key', 'r') as f:
 
 gs = Gradescope(login_info['username'], login_info['password'], verbose=True)
 
-courses = gs.get_courses(role=Role.INSTRUCTOR)
+courses = gs.get_courses(role=Role.INSTRUCTOR) # Instructor, Student, or Both
 save_json('./courses.data', courses, encoder=EnhancedJSONEncoder)
 
 course = courses[1]
@@ -15,27 +15,27 @@ print(course)
 assignments = gs.get_assignments(course)
 save_json('./assignments.data', assignments, encoder=EnhancedJSONEncoder)
 
-# members = gs.get_members(course)
-# save_json('./members.data', members, encoder=EnhancedJSONEncoder)
+members = gs.get_members(course)
+save_json('./members.data', members, encoder=EnhancedJSONEncoder)
 
 assignment = assignments[0]
 print(assignment)
+
+member = members[2]
+print(member)
+
+gradebook = gs.get_gradebook(course, member)
+save_json('./gradebook.data', gradebook, encoder=EnhancedJSONEncoder)
+
+past_submission = gs.get_past_submissions(course, assignment, member)
+save_json('./past_submission.data', past_submission, encoder=EnhancedJSONEncoder)
+
+grades_csv = gs.get_assignment_grades(assignment)
+save_csv('./assignment_grades_csv.data', grades_csv)
+
+print('Downloading...', past_submission[-1])
+gs.download_file('./submission.zip', past_submission[-1].get_file_url())
+
 assignment.release_date="2025-10-13T14:50"
 assignment.title="TestHomework2"
-
-testing = gs.update_assignment(assignment)
-
-# member = members[2]
-# print(member)
-
-# gradebook = gs.get_gradebook(course, member)
-# save_json('./gradebook.data', gradebook, encoder=EnhancedJSONEncoder)
-
-# past_submission = gs.get_past_submissions(course, assignment, member)
-# save_json('./past_submission.data', past_submission, encoder=EnhancedJSONEncoder)
-
-# grades_csv = gs.get_assignment_grades(assignment)
-# save_csv('./assignment_grades_csv.data', grades_csv)
-
-# print('Downloading...', past_submission[-1])
-# gs.download_file('./submission.zip', past_submission[-1].get_file_url())
+gs.update_assignment(assignment)
